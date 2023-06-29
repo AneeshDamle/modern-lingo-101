79a5940688193a679a25da66a514b2a4c1b84ae0
Aneesh Damle <aneesh.damle@protonmail.com>
2023-06-29 21:52:59 +0530

    Basic file structure inundated with htmls

---
+++ src/app.py
@@ -0,0 +1,31 @@
+# Modern Lingo Learner
+
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+
+LINGOS = [
+    "GG HOGAYA",
+    "LEGIT",
+    "RIZZZ",
+]
+
+@app.route("/")
+def index():
+    return render_template("index.html", lingos=LINGOS)
+
+@app.route("/lingo", methods=["POST"])
+def register():
+  # Validate request
+  if request.form.get("lingo") not in LINGOS:
+      return render_template("failure.html")
+  if (request.form.get("lingo") == "LEGIT"):
+      return render_template("legit.html")
+  if (request.form.get("lingo") == "GG HOGAYA"):
+      return render_template("gg_hogaya.html")
+  if (request.form.get("lingo") == "RIZZZ"):
+      return render_template("rizzz.html")
+
+
+

ef44711c92066fb1003f590b4158ec3a08c5415c
rachanagade <ruchugade@gmail.com>
2023-07-01 11:50:54 +0530

    Added PogChamp to Lingo

--- src/app.py
+++ src/app.py
@@ -6,9 +6,11 @@ app = Flask(__name__)
 
 
 LINGOS = [
-    "GG HOGAYA",
+    "GG",
     "LEGIT",
     "RIZZZ",
+    "POGCHAMP"
+
 ]
 
 @app.route("/")
@@ -16,16 +18,24 @@ def index():
     return render_template("index.html", lingos=LINGOS)
 
 @app.route("/lingo", methods=["POST"])
+
 def register():
+  
   # Validate request
   if request.form.get("lingo") not in LINGOS:
       return render_template("failure.html")
+  
   if (request.form.get("lingo") == "LEGIT"):
       return render_template("legit.html")
+  
   if (request.form.get("lingo") == "GG HOGAYA"):
       return render_template("gg_hogaya.html")
+  
   if (request.form.get("lingo") == "RIZZZ"):
       return render_template("rizzz.html")
+  
+  if (request.form.get("lingo") == "POGCHAMP"):
+      return render_template("pogchamp.html")
 
 
 
